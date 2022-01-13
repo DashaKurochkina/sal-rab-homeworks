dc9427cf4cc621f2ab8b1493a165f8f088b6363d
Darya Kurochkina <justicepersondk@gmail.com>
2022-01-13 16:20:36 +0500

    Task 4.1

--- task_4/solution.js
+++ task_4/solution.js
@@ -2,16 +2,16 @@
 
 function parseProducts(json) {
     // Аргументом функции является JSON
-
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
     
     let data = JSON.parse(json);
-    
-    return data.products;
+    let products = data.products;     
 }
+
+return parseProducts(products);
 
 // Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
@@ -24,15 +24,12 @@ function parseProducts(json) {
 function renderProductsCards(json) {
 
     clearProducts();
-
+    
     let products = parseProducts(json);
-
-    length = products.length;
-
-    for(let i = 0; i <= products.length - 1; i++) {
+    let length = products.length;
+    
+    for(let i = 0; i < length; i += 1) {
         
-        addProducts(products);
-
+        addProduct(products[i]);
     }
-
 }
